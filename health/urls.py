d7aa42b9e5362dd390ae62982db92aeb1c3a10f9
Mudjaheedoon1 <dembeleabdoulmalick649@gmail.com>
2025-12-04 13:03:22 +0100

    Ajustement

--- health/urls.py
+++ health/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from . import views
+from frontend import views
 
 urlpatterns = [
     path('api/heartbeat/', views.get_heartbeat, name='heartbeat_api'),
+    path('heartbeat/', views.get_heartbeat, name='heartbeat_api'),
 ]
